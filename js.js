549666324ae4af04185ddaca6987ca45fbc9f1c6
Diwaltry353 <118283061+Diwaltry353@users.noreply.github.com>
2022-11-16 13:54:19 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,15 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "./imag/menu_white_36dp.png";
+
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "./imag/close_white_36dp.png";
+        
+    }
+    
+
+    }
+    alert("ola mundo");
